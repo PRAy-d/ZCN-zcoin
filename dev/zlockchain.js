52907b09f2b27be00c67a28eac6ae12cc82646cd
PRAy-D <learn.pd9913@gmail.com>
2021-06-04 22:44:50 +0530

    init1

--- dev/zlockchain.js
+++ dev/zlockchain.js
@@ -2,3 +2,18 @@ function Zlockchain() {
     this.chain = [];
     this.newTransactions = [];
 }
+Zlockchain.prototype.createNewBlock = function (nonce, prevBlockHash, hash) {
+    const newBlock = {
+        index: this.chain.length + 1,
+        timestamp: Date.now(),
+        transactions: this.newTransactions,
+        nonce: nonce,
+        hash: hash,
+        prevBlockHash: prevBlockHash,
+    };
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
+}
+module.exports = Zlockchain;

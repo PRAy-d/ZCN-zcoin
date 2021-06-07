b9698b30b97196203e24bd42d6ad419b880c6452
PRAy-D <learn.pd9913@gmail.com>
2021-06-07 13:25:10 +0530

    init3

--- dev/test.js
+++ dev/test.js
@@ -1,7 +1,25 @@
 const Zlockchain = require('./zlockchain')
 const pray = new Zlockchain();
-pray.createNewBlock(314, 'GFEUHGHIHF', 'hfjdk574839fhd');
-pray.createNewBlock(314, 'GFEUHGHIHF', 'hfjdk574839fhd');
-pray.createNewBlock(314, 'GFEUHGHIHF', 'hfjdk574839fhd');
+//pray.hashBlock();
+const previousBlockHash = '87765DA6CCF0668238C1D27C35692E11';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'B4CEE9C0E5CD571',
+        recipient: '3A3F6E462D48E9',
+    },
+    {
+        amount: 10,
+        sender: 'B4CEE9C0E5CD571',
+        recipient: '3A3F6E462D48E9',
+    },
+    {
+        amount: 10,
+        sender: 'B4CEE9C0E5CD571',
+        recipient: '3A3F6E462D48E9',
+    }
 
+]
+const nonce = 100;
+console.log(pray.hashBlock(previousBlockHash, currentBlockData, nonce));
 console.log(pray);
--- dev/zlockchain.js
+++ dev/zlockchain.js
@@ -1,7 +1,8 @@
+const sha256 = require('sha256');
 function Zlockchain() {
     this.chain = [];
     this.pendingTransactions = [];
-}
+};
 Zlockchain.prototype.createNewBlock = function (nonce, prevBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
@@ -15,10 +16,10 @@ Zlockchain.prototype.createNewBlock = function (nonce, prevBlockHash, hash) {
     this.chain.push(newBlock);
 
     return newBlock;
-}
+};
 Zlockchain.prototype.getLastBlock = function () {
     return this.chain[this.chain.length - 1];
-}
+};
 Zlockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
     const newTransaction = {
         amount: amount,
@@ -26,6 +27,11 @@ Zlockchain.prototype.createNewTransaction = function (amount, sender, recipient)
         recipient: recipient,
     };
     this.pendingTransactions.push(newTransaction);
-    return.this.getLastBlock()['index'] + 1;
-}
+    return this.getLastBlock()['index'] + 1;
+};
+Zlockchain.prototype.hashBlock = function (prevBlockHash, currentBlockData, nonce) {
+    const blockdata = prevBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(blockdata);
+    return hash;
+};
 module.exports = Zlockchain;

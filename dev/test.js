1486c68181266ac3bac6f692d126fdd60a89b8a8
PRAy-D <learn.pd9913@gmail.com>
2021-06-07 10:45:31 +0530

    init2

---
+++ dev/test.js
@@ -0,0 +1,7 @@
+const Zlockchain = require('./zlockchain')
+const pray = new Zlockchain();
+pray.createNewBlock(314, 'GFEUHGHIHF', 'hfjdk574839fhd');
+pray.createNewBlock(314, 'GFEUHGHIHF', 'hfjdk574839fhd');
+pray.createNewBlock(314, 'GFEUHGHIHF', 'hfjdk574839fhd');
+
+console.log(pray);
--- dev/zlockchain.js
+++ dev/zlockchain.js
@@ -1,19 +1,31 @@
 function Zlockchain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 Zlockchain.prototype.createNewBlock = function (nonce, prevBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         prevBlockHash: prevBlockHash,
     };
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
+}
+Zlockchain.prototype.getLastBlock = function () {
+    return this.chain[this.chain.length - 1];
+}
+Zlockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient,
+    };
+    this.pendingTransactions.push(newTransaction);
+    return.this.getLastBlock()['index'] + 1;
 }
 module.exports = Zlockchain;
